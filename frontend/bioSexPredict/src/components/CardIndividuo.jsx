af8a08ba33a8715b68934916bac0cf486ffb0790
Livian Torres <liviantorres@alu.ufc.br>
2025-02-13 13:46:19 -0300

    Resolve #21

--- frontend/bioSexPredict/src/components/CardIndividuo.jsx
+++ frontend/bioSexPredict/src/components/CardIndividuo.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { useState } from "react";
+import ModalEditar from "./ModalExibir";
 
 const CardContainer = styled.div`
   background: #e0edff;
@@ -32,13 +34,99 @@ const InfoText = styled.p`
   width: 100%;
 `;
 
-const CardIndividuo = ({ img, id, sexo }) => {
+const individuos = [
+  {
+    img: "https://randomuser.me/api/portraits/women/1.jpg",
+    descricao:
+      "Professor de Matemática, com 10 anos de experiência no ensino superior.",
+    localizacao: "São Paulo, SP",
+    id: "PROF12345",
+    frontalS_A: "15cm",
+    frontalL_R: "10cm",
+    frontalS_R: "12cm",
+    maxilarDireitoS_I: "18cm",
+    frontalS_I: "14cm",
+    frontalS_L: "9cm",
+    frontalA_R: "7cm",
+    sexo: "Sexo Feminino",
+  },
+  {
+    cargo: "Servidor",
+    img: "https://randomuser.me/api/portraits/men/2.jpg",
+    descricao:
+      "Responsável pela logística do campus, com foco em atendimento a alunos e professores.",
+    localizacao: "Fortaleza, CE",
+    id: "SERV12345",
+    frontalS_A: "13cm",
+    frontalL_R: "8cm",
+    frontalS_R: "10cm",
+    maxilarDireitoS_I: "16cm",
+    frontalS_I: "12cm",
+    frontalS_L: "7cm",
+    frontalA_R: "6cm",
+    sexo: "Sexo Masculino",
+  },
+
+  {
+    cargo: "Estudante",
+    img: "https://randomuser.me/api/portraits/men/3.jpg",
+    descricao:
+      "Estudante de Engenharia de Software, interessado em Inteligência Artificial.",
+    localizacao: "Rio de Janeiro, RJ",
+    id: "ESTU12345",
+    frontalS_A: "14cm",
+    frontalL_R: "9cm",
+    frontalS_R: "11cm",
+    maxilarDireitoS_I: "17cm",
+    frontalS_I: "13cm",
+    frontalS_L: "8cm",
+    frontalA_R: "7cm",
+    sexo: "Sexo Masculino",
+  },
+  {
+    cargo: "Administrador",
+    img: "https://randomuser.me/api/portraits/women/4.jpg",
+    descricao:
+      "Gestora administrativa da instituição, com foco em melhoria contínua e processos organizacionais.",
+    localizacao: "Belo Horizonte, MG",
+    id: "ADM12345",
+    frontalS_A: "16cm",
+    frontalL_R: "10cm",
+    frontalS_R: "13cm",
+    maxilarDireitoS_I: "19cm",
+    frontalS_I: "15cm",
+    frontalS_L: "9cm",
+    frontalA_R: "8cm",
+    sexo: "Sexo Feminino",
+  },
+  { img: "/imagePerfil.svg", id: "ID1231FASF", sexo: "Sexo Feminino" },
+  { img: "/imagePerfil.svg", id: "ID1231FASF", sexo: "Sexo Masculino" },
+  { img: "/imagePerfil.svg", id: "ID1231FASF", sexo: "Sexo Masculino" },
+  { img: "/imagePerfil.svg", id: "ID1231FASF", sexo: "Sexo Masculino" },
+  { img: "/imagePerfil.svg", id: "ID1231FASF", sexo: "Sexo Masculino" },
+  { img: "/imagePerfil.svg", id: "ID1231FASF", sexo: "Sexo Masculino" },
+  { img: "/imagePerfil.svg", id: "ID1231FASF", sexo: "Sexo Feminino" },
+  { img: "/imagePerfil.svg", id: "ID1231FASF", sexo: "Sexo Masculino" },
+];
+
+const CardIndividuo = ({ counter, img, id, sexo }) => {
+  const [modal, setModal] = useState(false);
+
   return (
-    <CardContainer>
-      <ImagePlaceholder src={img} alt="Foto do indivíduo" />
-      <InfoText>{id}</InfoText>
-      <InfoText>{sexo}</InfoText>
-    </CardContainer>
+    <>
+      <CardContainer onClick={() => setModal(true)}>
+        <ImagePlaceholder src={img} alt="Foto do indivíduo" />
+        <InfoText>{id}</InfoText>
+        <InfoText>{sexo}</InfoText>
+      </CardContainer>
+
+      {modal && (
+        <ModalEditar
+          onClose={() => setModal(false)}
+          individuo={individuos[counter]}
+        />
+      )}
+    </>
   );
 };
 
--- frontend/bioSexPredict/src/pages/IndividuosCatalogados.jsx
+++ frontend/bioSexPredict/src/pages/IndividuosCatalogados.jsx
@@ -4,6 +4,8 @@ import { HiIdentification } from "react-icons/hi2";
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 import CardIndividuo from "../components/CardIndividuo";
 
+
+
 const ContainerPrincipal = styled.div`
   background-color: #1957a7;
   width: 100%;
@@ -68,10 +70,71 @@ const PageButton = styled.button`
 `;
 
 const individuos = [
-  { img: "/imagePerfil.svg", id: "ID1231FASF", sexo: "Sexo Masculino" },
-  { img: "/imagePerfil.svg", id: "ID1231FASF", sexo: "Sexo Masculino" },
-  { img: "/imagePerfil.svg", id: "ID1231FASF", sexo: "Sexo Masculino" },
-  { img: "/imagePerfil.svg", id: "ID1231FASF", sexo: "Sexo Masculino" },
+  {
+    nome: "Maria Silva",
+    img: "https://randomuser.me/api/portraits/women/1.jpg",
+    descricao: "Professor de Matemática, com 10 anos de experiência no ensino superior.",
+    localizacao: "São Paulo, SP",
+    id: "PROF12345",
+    frontalS_A: "15cm",
+    frontalL_R: "10cm",
+    frontalS_R: "12cm",
+    maxilarDireitoS_I: "18cm",
+    frontalS_I: "14cm",
+    frontalS_L: "9cm",
+    frontalA_R: "7cm",
+    sexo: "Sexo Feminino" 
+  },
+ {
+    nome: "Carlos Oliveira",
+    cargo: "Servidor",
+    img: "https://randomuser.me/api/portraits/men/2.jpg",
+    descricao: "Responsável pela logística do campus, com foco em atendimento a alunos e professores.",
+    localizacao: "Fortaleza, CE",
+    id: "SERV12345",
+    frontalS_A: "13cm",
+    frontalL_R: "8cm",
+    frontalS_R: "10cm",
+    maxilarDireitoS_I: "16cm",
+    frontalS_I: "12cm",
+    frontalS_L: "7cm",
+    frontalA_R: "6cm",
+    sexo: "Sexo Masculino" 
+  },
+  
+  {
+    nome: "Lucas Pereira",
+    cargo: "Estudante",
+    img: "https://randomuser.me/api/portraits/men/3.jpg",
+    descricao: "Estudante de Engenharia de Software, interessado em Inteligência Artificial.",
+    localizacao: "Rio de Janeiro, RJ",
+    id: "ESTU12345",
+    frontalS_A: "14cm",
+    frontalL_R: "9cm",
+    frontalS_R: "11cm",
+    maxilarDireitoS_I: "17cm",
+    frontalS_I: "13cm",
+    frontalS_L: "8cm",
+    frontalA_R: "7cm",
+    sexo: "Sexo Masculino"
+  },
+  {
+
+    nome: "Fernanda Souza",
+    cargo: "Administrador",
+    img: "https://randomuser.me/api/portraits/women/4.jpg",
+    descricao: "Gestora administrativa da instituição, com foco em melhoria contínua e processos organizacionais.",
+    localizacao: "Belo Horizonte, MG",
+    id: "ADM12345",
+    frontalS_A: "16cm",
+    frontalL_R: "10cm",
+    frontalS_R: "13cm",
+    maxilarDireitoS_I: "19cm",
+    frontalS_I: "15cm",
+    frontalS_L: "9cm",
+    frontalA_R: "8cm",
+    sexo: "Sexo Feminino"
+  },
   { img: "/imagePerfil.svg", id: "ID1231FASF", sexo: "Sexo Feminino" },
   { img: "/imagePerfil.svg", id: "ID1231FASF", sexo: "Sexo Masculino" },
   { img: "/imagePerfil.svg", id: "ID1231FASF", sexo: "Sexo Masculino" },
@@ -82,10 +145,12 @@ const individuos = [
   { img: "/imagePerfil.svg", id: "ID1231FASF", sexo: "Sexo Masculino" },
 ];
 
+
+
 const IndividuosCatalogados = () => {
+  const [modal, setModal] = useState(false);
   const itensPorPagina = 8;
   const [paginaAtual, setPaginaAtual] = useState(0);
-
   const inicio = paginaAtual * itensPorPagina;
   const fim = inicio + itensPorPagina;
   const individuosPaginados = individuos.slice(inicio, fim);
@@ -104,6 +169,7 @@ const IndividuosCatalogados = () => {
             id={individuo.id}
             sexo={individuo.sexo}
             img={individuo.img}
+            counter={index}
           />
         ))}
       </GridContainer>
@@ -123,6 +189,8 @@ const IndividuosCatalogados = () => {
           <FaArrowRight />
         </PageButton>
       </PaginationContainer>
+      {modal && <ModalEditar onClose={()=> setModal(false)}/>}
+      
     </ContainerPrincipal>
   );
 };
