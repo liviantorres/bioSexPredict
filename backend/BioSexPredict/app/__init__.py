693c2f4e4158c4edb9ca1c6f8c2843b42b17522e
Lucas <soares0lucas8@gmail.com>
2025-02-24 21:47:09 -0300

    chore: tratandp erro de cors

--- backend/BioSexPredict/app/__init__.py
+++ backend/BioSexPredict/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from app.database import init_db
 from app.routes import user_bp, auth_bp, individual_bp
 from flask_migrate import Migrate
@@ -12,6 +13,7 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = 'ce03a871-7d74-4992-a48c-6124cb43f617' 
     init_db(app)
+    CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
     app.register_blueprint(user_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(individual_bp)
